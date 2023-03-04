4274ab5bfc682382c61e289c90848f2099e97d6d
Mykyta Kovalenko <nikkv.ottf@gmail.com>
2023-03-04 09:15:37 +0100

    Lab3: + ability to mark tasks as done

--- Lab3/src/app.ts
+++ Lab3/src/app.ts
@@ -35,6 +35,13 @@ app.post('/deleteTask', (req: Request, res: Response) => {
     res.sendStatus(200);
   });
 
+app.post('/tickTask', (req: Request, res: Response) => {
+    const { index } = req.body;
+    const task =  tasksList[index];
+    task.isDone = !task.isDone;
+    res.sendStatus(200);
+  });
+
 app.listen(8080, () =>{
     console.log("Server is listening on port 8080" )
     const task1 = new Task('Text of the task', Priority.Low, new Date())
--- Lab3/src/entities/task.ts
+++ Lab3/src/entities/task.ts
@@ -1,17 +1,51 @@
-import internal from "stream";
-import {Priority} from '../enums/priority'
+import { Priority } from '../enums/priority'
 
-export class Task{
-    text: string;
-    priority: Priority;
-    date: Date
+export class Task {
+    private _text: string;
+    private _priority: Priority;
+    private _date: Date
+    private _isDone: boolean = false;
 
-    constructor(text:string, priority: Priority, date: Date){
-        this.text = text;
-        this.priority = priority;
-        this.date = date;
+    constructor(text: string, priority: Priority, date: Date) {
+        this._text = text;
+        this._priority = priority;
+        this._date = date;
     }
-    getInfo(): string{
+
+    get text(): string {
+        return this._text;
+    }
+
+    set text(value: string) {
+        this._text = value;
+    }
+
+    get priority(): Priority {
+        return this._priority;
+    }
+
+    set priority(value: Priority) {
+        this._priority = value;
+    }
+
+    get date(): Date {
+        return this._date;
+    }
+
+    set date(value: Date) {
+        this._date = value;
+    }
+
+
+    get isDone(): boolean {
+        return this._isDone;
+    }
+
+    set isDone(value: boolean) {
+        this._isDone = value;
+    }
+
+    getInfo(): string {
         return this.text + ' ' + this.priority + ' ' + this.date.toLocaleString()
     }
 }
