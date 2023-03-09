15d035730d8de8be1a98ae2d0bd1dfb6df496f99
Danil <danilsmy.edu@gmail.com>
2023-03-09 12:42:21 +0200

    Lab3: add sotring by priority or date of creation

--- Lab3/src/app.ts
+++ Lab3/src/app.ts
@@ -1,5 +1,5 @@
-import express, {Express, Request, Response} from 'express';
-import {Task} from '../src/entities/task'
+import express, { Express, Request, Response } from 'express';
+import { Task } from '../src/entities/task'
 import { Priority } from './enums/priority';
 import { tasksList } from './storage/data';
 import bodyParser from 'body-parser'
@@ -8,20 +8,34 @@ const app: Express = express()
 
 app.set('view engine', 'ejs')
 app.set('views', 'src/views/pages')
-app.use(express.urlencoded({extended: true})); 
-app.use(express.json());   
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.get('/',function(req: Request, res:Response){
-    res.render('index', {tasksList: tasksList});
+app.get('/', function (req: Request, res: Response) {
+    res.render('index', { tasksList: tasksList });
 });
 
-app.get('/create',function(req: Request, res:Response){
+app.get('/create', function (req: Request, res: Response) {
     res.render('create');
 });
 
-app.post('/create',function(req: Request, res:Response){
+app.post('/tasks', (req: Request, res: Response) => {
+    const sortField: string = req.body.byField;
+    const sortedTasks = tasksList.sort((taskA, taskB) => {
+        if (taskA[sortField] < taskB[sortField]) {
+            return -1;
+        } else if (taskA[sortField] > taskB[sortField]) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+    res.render('index', { tasksList: sortedTasks });
+})
+
+app.post('/create', function (req: Request, res: Response) {
     const taskText: string = req.body.taskText;
     const priority: string = Priority[req.body.priority];
     const newTask: Task = new Task(taskText, Priority[priority], new Date());
@@ -34,17 +48,15 @@ app.post('/deleteTask', (req: Request, res: Response) => {
     const { index } = req.body;
     tasksList.splice(index, 1);
     res.sendStatus(200);
-  });
+});
 
 app.post('/tickTask', (req: Request, res: Response) => {
     const { index } = req.body;
-    const task =  tasksList[index];
+    const task = tasksList[index];
     task.isDone = !task.isDone;
     res.sendStatus(200);
-  });
+});
 
-app.listen(8080, () =>{
-    console.log("Server is listening on port 8080" )
-    const task1 = new Task('Text of the task', Priority.Low, new Date())
-    console.log(task1.getInfo())
+app.listen(8080, () => {
+    console.log("Server is listening on port 8080")
 })
